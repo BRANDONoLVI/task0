f526b874237355e17755929b271dad93bcbaedbd
Brandon Kong <kongbrandon0@gmail.com>
2025-05-07 01:11:05 -0500

    feat: added mouse

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,16 +6,27 @@ import (
     "fmt"
     "syscall"
 	"mouse-service/v1/pkg/dbus"
+    "mouse-service/v1/internal/hardware"
 )
 
 func main() {
 	fmt.Println("Registering D-Bus service...")
+    mousConn, err := dbus.RegisterMouseService()
+    if err != nil {
+        fmt.Println("D-Bus Mouse registration failed:", err)
+        return
+    }
+	defer mousConn.Close()
+
 	conn, err := dbus.RegisterActionService()
     if err != nil {
         fmt.Println("D-Bus registration failed:", err)
         return
     }
 	defer conn.Close()
+
+    // Read mouse inputs here
+    hardware.ReadMouseEvents()
 
     fmt.Println("Service running. Press Ctrl+C to exit.")
     c := make(chan os.Signal, 1)
